e793a3fcaf5bd514a0e6cd4b9ede274a152b39e7
Skye <2018389656@ufs4life.ac.za>
2026-01-26 14:59:05 +0200

    Created a simmple calculator to demonstrate c# as a domain model

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+Console.WriteLine("Enter the first number:");
+int input1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second number:");
+int input2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter an operation (+, -, *, /):");
+string operation = Console.ReadLine();
+
+ /*switch(operation)
+{
+    case "+":
+        Console.WriteLine($"Result: {input1 + input2}");
+        break;
+    case "-":
+        Console.WriteLine($"Result: {input1 - input2}");
+        break;
+    case "*":
+        Console.WriteLine($"Result: {input1 * input2}");
+        break;
+    case "/":
+        if (input2 != 0)
+        {
+            Console.WriteLine($"Result: {input1 / input2}");
+        }
+        else
+        {
+            Console.WriteLine("Error: Division by zero is not allowed.");
+        }
+        break;
+    default:
+        Console.WriteLine("Error: Invalid operation.");
+        break;
+} */
+
+if(operation == "+")
+{
+    Console.WriteLine($"Result: {input1 + input2}");
+}
+else if(operation == "-")
+{
+    Console.WriteLine($"Result: {input1 - input2}");
+}
+else if(operation == "*")
+{
+    Console.WriteLine($"Result: {input1 * input2}");
+}
+else if(operation == "/")
+{
+   
+        Console.WriteLine($"Result: {input1 / input2}");
+   
+}
+else
+{
+    Console.WriteLine("Error: Invalid operation.");
+}
