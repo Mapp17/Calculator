61d6b9126efea15362eebd43b549be5e7eb5e654
Masoka Andile Mohono <masokaandile17@gmail.com>
2026-02-04 12:55:11 +0200

    Refactor project paths and update dependencies for CalculatorDomain and CalculatorDomainDemo
    - Updated project paths in project.assets.json, project.nuget.cache, and various .csproj files to reflect new directory structure.
    - Changed package folder paths to point to the new user directory.
    - Updated assembly informational version attributes in AssemblyInfo.cs files for both projects.
    - Modified sourcelink.json files to reference the correct GitHub commit hashes.
    - Updated binary files for CalculatorDomain.dll, api.dll, and associated PDB files to ensure compatibility with the new structure.

--- obj/Debug/net8.0/CalculatorDomainDemo.AssemblyInfo.cs
+++ obj/Debug/net8.0/CalculatorDomainDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorDomainDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d5b0376b82e5cd0bb758fcee4ab8c6b9213c3f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d83344cdc6ee7b1e5d660e00a45cc5ac4f088478")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorDomainDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorDomainDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
